dadea02ac0c56b68a873df06095bfbbeac1893a3
Vasile Rares-Mihail <75566187+vasile-rares@users.noreply.github.com>
2025-11-13 16:38:38 +0200

    feat: CMakeLists file addition and launch settings

--- GameUI/UI/HUD.h
+++ GameUI/UI/HUD.h
@@ -2,10 +2,9 @@
 
 #include "raylib.h"
 
-class HUD
-{
-public:
+class HUD {
+   public:
     static void DrawDebugInfo(int posX, int posY);
-    
+
     static void DrawTitle(const char* title, int posX, int posY, int fontSize, Color color);
 };
